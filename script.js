c5167ccd7af726dbed039ceed34d64ac7c465746
saracsbarbosa <sarasbarbosa.8@gmail.com>
2025-11-10 16:31:46 -0300

    adicionei um botao e 3 tags

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const titulo = document.getElementById('texto').innerText;
-console.log(titulo)
-alert("O texto é: " + titulo)
+
+const campoNome = document.getElementById("campo-nome");
+campoNome.addEventListener("input", (e) => {
+    console.log(e.target.value);
+});
+
+const caixa = document.getElementById("caixa");
+// caixa.innerHTML = "<h2>Conteúdo adicionado via JavaScript</h2>";
+
+const botao = document.getElementById("bnt");
+const mensagem = document.getElementById("mensagem");
+botao.addEventListener("click", (e) => {
+    const valor = campoNome.value;
+    mensagem.textContent = "Oiii " + valor;
+});
